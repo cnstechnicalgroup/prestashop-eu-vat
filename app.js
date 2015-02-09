71de5a05b62e459736539c02a31971dd0ab0b10d
Sam Morrison <sam@cnstechgroup.com>
2015-02-09 06:05:39 +0000

    cleaned up promise code

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
 'use strict'
 
-//var mysql = require("mysql");
 var Promise = require("bluebird");
 var mysql = require('mysql-promise2');
-
-/*
-Promise.promisifyAll(mysql);
-Promise.promisifyAll(require("mysql/lib/Connection").prototype);
-Promise.promisifyAll(require("mysql/lib/Pool").prototype);*/
 var using = Promise.using;
 var config = require('./config').config;
+var pool = mysql.createPool(config.db_config);
 var tax_rules_group = config.tax_rules_group;
 var countries = [];
 var specificPriceInserts = [];
-
-// Create MySQL connection
-var pool = mysql.createPool(config.db_config);
-/*var getConnection = function() {
-    return pool.getConnectionAsync().disposer(function(connection) {
-        connection.release();
-    });
-}*/
 
 //
 // All SQL here
 //
 
 // SQL: Return all products to get id_product and id_tax_rules_group
-var productSQL = 'SELECT id_product, price FROM prestashop.ps_product WHERE id_product IN (3, 4, 5); ';
+var productSQL = "SELECT id_product, price FROM prestashop.ps_product; ";
 
 // SQL: Get all EU country IDs
 var euCountryIDsSQL = "SELECT tr.id_country, t.rate FROM ps_tax_rule AS tr JOIN ps_tax AS t ON t.id_tax = tr.id_tax WHERE tr.id_tax_rules_group = " + tax_rules_group + " AND tr.id_tax = 5; "
@@ -36,51 +23,25 @@ var euCountryIDsSQL = "SELECT tr.id_country, t.rate FROM ps_tax_rule AS tr JOIN 
 var productAndCountrySQL = productSQL + euCountryIDsSQL;
 
 // SQL: Set all product id_tax_rules_group to 7
-var idTaxRulesGroupSQL = "UPDATE ps_product SET id_tax_rules_group = " + tax_rules_group + " WHERE id_product IN (3, 4, 5); UPDATE ps_product_shop SET id_tax_rules_group = " + tax_rules_group + " WHERE id_product IN (3, 4, 5);";
+var idTaxRulesGroupSQL = "UPDATE ps_product SET id_tax_rules_group = " + tax_rules_group + "; UPDATE ps_product_shop SET id_tax_rules_group = " + tax_rules_group + ";";
 
 // SQL: Update existing specific price
 var updateSpecificPriceSQL = "UPDATE ps_specific_price SET price = ? WHERE id_product = ? AND id_country = ?";
 
 // SQL: Insert new specific price
-var insertSQL = "INSERT INTO ps_specific_price ? ";
+var insertSQL = "INSERT INTO ps_specific_price SET ? ";
 
 // Set all product id_tax_rules_group to config.tax_rules_group value
-/*connection.query(idTaxRulesGroupSQL, function(err, result) {
-    if (err) {
-        connection.rollback(function() {
-            throw err;
-        });
-    }
-});*/
-/*
-// Create a json object for each new insert
-var createSpecificPriceInserts = function(product, countries) {
-    countries.forEach(function(country) {
-        var specificPrice = {
-            id_specific_price_rule: 0,
-            id_cart: 0,
-            id_product: product.id_product,
-            id_shop: 0,
-            id_shop_group: 0,
-            id_currency: 0,
-            id_country: country.id_country,
-            id_group: 0,
-            id_customer: 0,
-            id_product_attribute: 0,
-            price: (country.id_country == 0 ? (product.price + (product.price * country.rate)).toFixed(2) : -1),
-            from_quantity: 1,
-            reduction: 0,
-            reduction_tax: 1,
-            reduction_type: 'amount',
-            from: '0000-00-00 00:00:00',
-            to: '0000-00-00 00:00:00'
-        }
 
-        // Add specific price to object
-        specificPriceInserts.push(specificPrice);
-    });
-}
+using(pool.getTransaction(), function(connection) {
+  connection.queryAsync(idTaxRulesGroupSQL).catch(function(err) {
+      connection.rollback(function() {
+        throw err;
+      });
+  });
+});
 
+// Format a single specific price row
 var createSpecificPriceInsert = function(product, country) {
     var specificPrice = {
         id_specific_price_rule: 0,
@@ -104,37 +65,8 @@ var createSpecificPriceInsert = function(product, country) {
     return specificPrice;
 }
 
-// Update the specific price with the new flat rate
-var updateSpecificPrice = function(row) {
-    return using(getConnection(), function(connection) {
-        return connection.queryAsync("UPDATE ps_specific_price SET price = " + row.price + " WHERE id_product = " + row.id_product + " AND id_country = " + row.id_country).then(function(results) {
-            return results;
-        }).
-        catch (function(err) {
-            connection.rollback();
-            throw err;
-        });
-    });
-};
-
-// The ps_specific_price table doesn't have a unique
-// key on the multi-key index which includes id_product
-// and id_country. This INSERT INTO prohibits duplicates.
-var insertSpecificPrice = function(row) {
-    return using(getConnection(), function(connection) {
-        var insertSQL = "INSERT INTO ps_specific_price ? "
-        return connection.queryAsync(insertSQL, row).then(function(result) {
-            console.log(connection);
-            return result;
-        }).
-        catch (function(err) {
-            connection.rollback();
-            throw err;
-        });
-    });
-}
-*/
-
+// Create specificPriceRows object containing all possible
+// specific price entries / updates.
 var stageSpecificPrices = function(products, countries) {
 
     var specificPriceRows = [];
@@ -152,7 +84,6 @@ var stageSpecificPrices = function(products, countries) {
             specificPriceRows.push(createSpecificPriceInsert(product, country));
         });
     });
-
     return specificPriceRows;
 }
 
@@ -162,7 +93,6 @@ var stageSpecificPrices = function(products, countries) {
 // entry with the flat price that includes the EU VAT rate.
 //
 
-//using(getConnection(), function(connection) {
 using(pool.getTransaction(), function(connection) {
     // this callback is still pending
     return connection.queryAsync(productAndCountrySQL).then(function(results) {
@@ -171,22 +101,20 @@ using(pool.getTransaction(), function(connection) {
         countries = results[0][1];
         // Return the products results
         return stageSpecificPrices(products, countries);
-    }).all(function(row) {
+    }).each(function(row) {
         // For each specific price, try to update it or insert as new
         var update_values = [row.price, row.id_product, row.id_country];
-        return connection.queryAsync(updateSpecificPriceSQL, update_values).then(function(results) {
-            if (results.affectedRows == 0) {
-                return connection.queryAsync(insertSQL, row);
+        connection.queryAsync(updateSpecificPriceSQL, update_values).then(function(results) {
+            if (results[0].affectedRows === 0) {
+                connection.queryAsync(insertSQL, row);
             }
         });
-    }).
-    catch (function(err) {
+    }).catch (function(err) {
         console.log(err);
         connection.rollback();
         throw err;
     });
-}).
-finally(function() {
+}).finally(function() {
     // All done, close the pool connection
     pool.end(function() {
         console.log("all done.");
